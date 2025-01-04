c7eaa22d0d098681303e9898fcbaf6aa9939844c
Nalzurin <alexnormandi@ukr.net>
2025-01-04 20:57:24 +0200

    Add mod files

---
+++ Source/MainButtonWorker_EntityCodexTab.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Sound;
+
+namespace EntityCodexFixed
+{
+    public class MainButtonWorker_AnomalyCodexTab : MainButtonWorker
+    {
+        public override bool Disabled
+        {
+            get
+            {
+                if (base.Disabled)
+                {
+                    return true;
+                }
+                if (!ModsConfig.AnomalyActive)
+                {
+                    return true;
+                }
+                if(Find.Anomaly.Level == 0 && !EntityCodexFixed_Settings.alwaysShow)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public override void Activate()
+        {
+            Find.WindowStack.Add(new Dialog_EntityCodex());
+        }
+        public override bool Visible => !Disabled;
+    }
+
+
+}
---
+++ Source/Mod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace EntityCodexFixed
+{
+    public class EntityCodexFixed_Mod : Mod
+    {
+        public static EntityCodexFixed_Settings settings;
+
+        public EntityCodexFixed_Mod(ModContentPack content)
+            : base(content)
+        {
+            settings = GetSettings<EntityCodexFixed_Settings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Fixed! - Entity Codex";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            settings.DoWindowContents(inRect);
+        }
+    }
+}
