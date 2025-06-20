b9832090116320a55639cb29eed6088a45fe7d26
Nalzurin <alexnormandi@ukr.net>
2025-06-21 01:01:58 +0300

    Update to 1.6

---
+++ 1.6/Source/Harmony.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+namespace EntityCodexFixed
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyStarter
+    {
+        static HarmonyStarter()
+        {
+            Harmony harmony = new Harmony("EntityCodexFixed");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    public static class Patch_PlaySettingsRemoveAnomalyCodex
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(PlaySettings), "DoPlaySettingsGlobalControls");
+        }
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+
+                if (instruction.opcode == OpCodes.Call && instruction.Calls(AccessTools.Method(typeof(ModsConfig), "get_AnomalyActive")))
+                {
+                    CodeInstruction instr = new CodeInstruction(OpCodes.Ldc_I4_0);
+                    instr.labels = instruction.ExtractLabels();
+                    yield return instr;
+                    continue;
+                }
+                yield return instruction;
+            }
+        }
+
+    }
+}
---
+++ 1.6/Source/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace EntityCodexFixed
+{
+    public class EntityCodexFixed_Settings : ModSettings
+    {
+        public static bool alwaysShow = true;
+
+        public override void ExposeData()
+        {
+
+            Scribe_Values.Look(ref alwaysShow, "alwaysShow", defaultValue: true, forceSave: true);
+
+
+            base.ExposeData();
+        }
+
+        public void DoWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.CheckboxLabeled("EntityCodexFixed_AlwaysShowButton".Translate(), ref alwaysShow);
+            listing_Standard.End();
+        }
+    }
+}
